eb251cf6b7877783765c1d0398312f3c18bdce43
Dawid Wojda <dawiidio@gmail.com>
2018-01-02 17:46:11 +0100

    Prepare build for export as npm module

--- webpack.plugin.js
+++ webpack.plugin.js
@@ -24,7 +24,7 @@ const config = {
       use: {
         loader: 'babel-loader',
         options: {
-          presets: ['env'],
+          // presets: ['env'],
           plugins: ["transform-object-rest-spread"]
         }
       }
